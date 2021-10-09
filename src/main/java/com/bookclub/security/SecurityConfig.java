2fa37629fa8177c22a0f1560a641913e1f8fcc3e
Naomi See <naomi.see@seenaomi.net>
2021-10-09 17:05:50 -0500

    Pushing up week 6 adding login form auth and http security functionality

---
+++ src/main/java/com/bookclub/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.bookclub.security;
+
+/*
+Lomax, R. (2021). CIS 530 Server-Side Development. Bellevue University.
+Modified by N.See 2021
+*/
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("seenaomi").password(encoder.encode("pass2021")).roles("USER", "ADMIN");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .logoutSuccessUrl("/login?logout=true")
+                    .invalidateHttpSession(true)
+                    .permitAll();
+
+    }
+}
